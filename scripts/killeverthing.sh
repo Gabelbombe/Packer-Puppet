7861dfcfda7d55a64e3dcf9357ff27dccfbf3254
ehime <dodomeki@gmail.com>
2015-07-06 18:53:24 -0700

    Removing relics

--- scripts/killeverthing.sh
+++ scripts/killeverthing.sh
@@ -5,17 +5,18 @@ if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as su" 1>&2 ; exit 1
 fi
 
+USER=jd_daniel
+
 ## Die die die die die
 for pid in $(ps aux |grep -i 'virtual\|vbox' |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 for pid in $(ps aux |grep vmware             |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 for pid in $(ps aux |grep parallel           |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 
 for item in $(prlctl list -a |tail -n1 |awk '{print$1}' |tr -d '{}'); do
-  echo  prlctl unregister $item
+  [ 'UUID' == $item ] && { break; } || { prlctl unregister $item; }
 done
 
-rm -fr /Users/jd_daniel/VirtualBox\ VMs/*
-
+su $USER -c "rm -fr ~/VirtualBox\ VMs/* ../packer/output/*"
 
 [ -f 'killvbox.sh' ] && {
   bash killvbox.sh
