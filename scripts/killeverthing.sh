e0d74b5c4993040b3c600467939346098539fac3
ehime <dodomeki@gmail.com>
2015-07-06 18:36:36 -0700

    Destroy nested boxes

--- scripts/killeverthing.sh
+++ scripts/killeverthing.sh
@@ -10,6 +10,9 @@ for pid in $(ps aux |grep -i 'virtual\|vbox' |grep -v grep |awk '{print$2}'); do
 for pid in $(ps aux |grep vmware             |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 for pid in $(ps aux |grep parallel           |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 
+rm -fr "/Users/jd_daniel/VirtualBox VMs/*"
+
+
 [ -f 'killvbox.sh' ] && {
   bash killvbox.sh
 }
