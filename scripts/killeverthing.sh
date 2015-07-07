fc7fa00c4f310e2dc05cd15f5303974a08cdb8d8
ehime <dodomeki@gmail.com>
2015-07-06 19:14:22 -0700

    Merging in kill scriptws

--- scripts/killeverthing.sh
+++ scripts/killeverthing.sh
@@ -1,25 +1,21 @@
 #!/bin/bash -eux
 
-## ROOT check
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as su" 1>&2 ; exit 1
-fi
-
-USER=jd_daniel
-
 ## Die die die die die
 for pid in $(ps aux |grep -i 'virtual\|vbox' |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 for pid in $(ps aux |grep vmware             |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
 for pid in $(ps aux |grep parallel           |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
-
-for item in $(prlctl list -a |tail -n1 |awk '{print$1}' |tr -d '{}'); do
-  [ 'UUID' == $item ] && { break; } || { prlctl unregister $item; }
-done
-
-su $USER -c "rm -fr ~/VirtualBox\ VMs/* ../packer/output/*"
-
-[ -f 'killvbox.sh' ] && {
-  bash killvbox.sh
-}
-
 for pid in $(ps aux |grep packer             |grep -v grep |awk '{print$2}'); do sudo kill -9 $pid; done
+
+
+while read -r name; do
+  VBoxManage unregistervm "$(echo $name |awk '{print$2}' |sed -e 's/.*{\([^}]\+\)}.*/\1/g')" --delete 2>&1 /dev/null
+done < <(VBoxManage list vms)
+
+
+while read -r name; do
+  prlctl unregister  $(echo $name awk '{print$1}' |sed -e 's/.*{\([^}]\+\)}.*/\1/g')
+done < <(prlctl list -a |tail -n +2)
+
+
+rm -fr ~/VirtualBox\ VMs/* ../packer/output/*
+
